7f88d627ba8f513d7c7c19302e48441f9260f88a
Tan-Thai <Vi.Tan.Thai@gmail.com>
2025-01-08 10:34:29 +0100

    feat(Plugins): Added Jacoco and Junit.Jupiter to project
    Added skeleton file on top of plugins for the current existing java file

---
+++ src/test/java/se/iths/FileStorageTest.java
@@ -0,0 +1,24 @@
+package se.iths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileStorageTest {
+
+    @Test
+    void createRecordTest() {
+    }
+
+    @Test
+    void readRecordTest() {
+    }
+
+    @Test
+    void getRecordIDsTest() {
+    }
+
+    @Test
+    void deleteRecordTest() {
+    }
+}
